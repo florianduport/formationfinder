47e88de3f3d2333abfb3e65b5536f2bbaea8aa4a
Dionis <inoid2007@gmail.com>
2016-09-16 19:52:50 -0500

    New updates2

--- front/src/app/adminplatform/js/formationfinder/config.js
+++ front/src/app/adminplatform/js/formationfinder/config.js
@@ -5,7 +5,7 @@ var config_module = angular.module('app.config', [])
     .constant('CONFIG', {
         'SYSTEM_NAME' : 'Formationfinder',
         'GOOGLE_ANALYTICS_ID' : '',
-        'BASE_URL' : 'http://localhost:1337',
+        'BASE_URL' : 'http://137.74.172.220:1337',
         'API_URL' : 'http://localhost:1337',
         'SYSTEM_LANGUAGE' : 'en'
     });
